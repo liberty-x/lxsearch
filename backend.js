b11e02b8a886ebe685d85a9b7702a9885558d045
Ruth <emailtoruth@virginmedia.com>
2015-09-30 19:45:32 +0100

    recieving data from api

--- backend.js
+++ backend.js
@@ -1,23 +1,27 @@
-var http = require('http');
-var port = 8000;
-var server = http.createServer(handler);
-server.listen(port);
-console.log("Server running at http://localhost:" + port);
+var wordSearch = 'house'
+var request = require('request');
+var env = require('env2')('./config.env');
+// var exports = module.exports
 
-
-
-var words = {
-  method: 'GET',
-  url: '/'
-}
-
-function handler(req, res) {
-      if (req.method === 'GET') {
-       res.writeHead(200, {"Content-Type": "text/html"});
-       res.end(words.text);
+request('http://api.wordnik.com:80/v4/word.json/' +wordSearch + '/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=' + process.env.APIkey, function (error, response, array) {
+    if (!error && response.statusCode == 200) {
+        var jsonObject = JSON.parse(array);
+        jsonObject[0].text
     }
-};
+});
 
 
+var wordOfTheDay = (function (){
+  var date = new Date();
+  var dt = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
 
-// var url = "http://api.wordnik.com:80/v4/word.json/grass/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=fc3e91dd61224bded00070e481e095e13cee83333b434f66e"
+  request('http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=' + dt + '&api_key=' + process.env.APIkey, function (error, response, array) {
+    if (!error && response.statusCode == 200) {
+        var jsonObject = JSON.parse(array);
+        console.log(jsonObject.word);
+        console.log(jsonObject.definitions[0].text);
+    }
+  });
+}());
+
+module.exports = wordOfTheDay;
