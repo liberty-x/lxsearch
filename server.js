96cef01bbe0bd7ad2729e4a49c756ebeb5812243
Ruth <emailtoruth@virginmedia.com>
2015-10-01 19:16:19 +0100

    dealing with front end request

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-//var definition = require('./backend.js');
+var apiReq = require('./backend.js');
+
 
 var serve = (function(){
   var http = require('http');
-  var port = 8000;
-  console.log("Server running at http://localhost:" + port);
+  var port = process.env.PORT || 8000;
+    console.log("Server running at http://localhost:" + port);
+  var fs = require('fs');
+
+  var index = fs.readFileSync(__dirname + 'index.html');
+
   function handler(req, res) {
-    //console.log(res);
+    var url = req.url;
+    console.log(url);
+    if (url === '/') {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(index.toString());
+    } else if (url.length > 1) {
+      wordDefintion(req, res);
+    };
+  };
+
+  function extractWord (str) {
+    return str.split('/')[1];
+  };
+
+  function wordDefintion (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end();
-  }
+    var word = extractWord(req.url);
+    apiReq(word, function(definition) {
+      res.end(defintion);
+    });
+  };
+
   var create = function(){
     var server = http.createServer(handler);
     server.listen(port);
---
+++ wordoftheday.js
@@ -0,0 +1,17 @@
+request = require('request'),
+env = require('env2')('./config.env');
+
+var wordOfTheDay = function(word, callback) {
+  var date = new Date();
+  var dt = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+  request('http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=' + dt + '&api_key=' + process.env.APIkey, function(error, response, array) {
+    if (!error && response.statusCode == 200) {
+      var jsonObject = JSON.parse(array);
+      callback(jsonObject.word);
+      // console.log (jsonObject.definitions[0].text);
+    }
+  });
+};
+
+module.exports = wordOfTheDay;
